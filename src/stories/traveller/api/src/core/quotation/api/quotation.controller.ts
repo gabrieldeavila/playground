b51db758faa3604c75199d19d6ab154a519fb23e
geavila <gabrielavila04@protonmail.com>
2024-05-02 23:27:29 -0300

    feat: :sparkles: start crawling the web

--- src/stories/traveller/api/src/core/quotation/api/quotation.controller.ts
+++ src/stories/traveller/api/src/core/quotation/api/quotation.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get, HttpCode, Inject, Logger } from "@nestjs/common";
+import { PuppeteerService } from "./quotation.service";
 
 @Controller("quotation")
 export class QuotationController {
-  constructor(@Inject(Logger) private readonly logger: Logger) {}
+  constructor(
+    @Inject(Logger) private readonly logger: Logger,
+    private readonly puppeteerService: PuppeteerService,
+  ) {}
 
   @Get()
   @HttpCode(200)
   run() {
-    this.logger.log("Quoatation endpoint called!");
+    this.puppeteerService.crawl();
+
     return { status: "hohohohaaa" };
   }
 }
--- src/stories/traveller/api/src/core/quotation/quotation.module.ts
+++ src/stories/traveller/api/src/core/quotation/quotation.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 
 import { QuotationController } from "./api/quotation.controller";
+import { PuppeteerService } from "./api/quotation.service";
 
 @Module({
   controllers: [QuotationController],
+  providers: [PuppeteerService],
 })
 export class QuoatationModule {}
