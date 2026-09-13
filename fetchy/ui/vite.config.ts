a98f56df4344bff35f3f8bc910304878e917776c
geavila <gabrielavila04@protonmail.com>
2026-09-12 23:40:42 -0300

    feat: :rocket: start fetchy

---
+++ fetchy/ui/vite.config.ts
@@ -0,0 +1,19 @@
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  
+
+  return {
+    plugins: [tailwindcss(), reactRouter()],
+    resolve: {
+      tsconfigPaths: true,
+    },
+    server: {
+      port: Number(env.PORT) || 5173,
+      host: env.HOST || true,
+    },
+  };
+});
